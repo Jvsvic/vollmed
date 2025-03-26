fd768820748a76a898f32cc822b5a0d66b7bf616
Jvsvic <vitor-oliveira001@hotmail.com>
2025-03-26 07:31:06 -0300

    Adicionar novas dependências no projeto; Mapear uma entidade JPA e criar uma interface Repository para ela; Utilizar o Flyway como ferramenta de Migrations do projeto; Realizar validações com Bean Validation utilizando algumas de suas anotações, como a @NotBlank.

--- src/main/java/estudos/vollmed/controller/MedicoController.java
+++ src/main/java/estudos/vollmed/controller/MedicoController.java
@@ -1,6 +1,12 @@
 package estudos.vollmed.controller;
 
+import estudos.vollmed.endereco.Endereco;
 import estudos.vollmed.medicodto.DadosCadastroMedico;
+import estudos.vollmed.medicodto.Medico;
+import estudos.vollmed.medicodto.Medicorepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private Medicorepository medicorepository;
+
+
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        medicorepository.save(new Medico(dados));
 
 
     }
---
+++ src/main/java/estudos/vollmed/medicodto/Medicorepository.java
@@ -0,0 +1,7 @@
+package estudos.vollmed.medicodto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Medicorepository extends JpaRepository<Medico, Long> {
+
+}
