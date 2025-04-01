b5c820f8d42392e7018cd2165cd0696cc9be9f46
Jvsvic <vitor-oliveira001@hotmail.com>
2025-03-31 21:11:16 -0300

    Utilizar a anotação @GetMapping para mapear métodos em Controllers que produzem dados; Utilizar a interface Pageable do Spring para realizar consultas com paginação; Controlar a paginação e a ordenação dos dados devolvidos pela API com os parâmetros page, size e sort; Configurar o projeto para que os comandos SQL sejam exibidos no console.

--- src/main/java/estudos/vollmed/controller/MedicoController.java
+++ src/main/java/estudos/vollmed/controller/MedicoController.java
@@ -2,15 +2,18 @@ package estudos.vollmed.controller;
 
 import estudos.vollmed.endereco.Endereco;
 import estudos.vollmed.medicodto.DadosCadastroMedico;
+import estudos.vollmed.medicodto.DadosListagemMedico;
 import estudos.vollmed.medicodto.Medico;
 import estudos.vollmed.medicodto.Medicorepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,10 +28,13 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         medicorepository.save(new Medico(dados));
-
-
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicorepository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
+    }
 
 
 
