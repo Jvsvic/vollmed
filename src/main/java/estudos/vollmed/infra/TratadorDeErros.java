329fe40761ee9ab50be512a97fa9ec24a70a2873
Jvsvic <vitor-oliveira001@hotmail.com>
2025-05-13 20:16:52 -0300

    Criar uma classe para isolar o tratamento de exceptions da API, com a utilização da anotação @RestControllerAdvice; Utilizar a anotação @ExceptionHandler, do Spring, para indicar qual exception um determinado método da classe de tratamento de erros deve capturar; Tratar erros do tipo 404 (Not Found) na classe de tratamento de erros; Tratar erros do tipo 400 (Bad Request), para erros de validação do Bean Validation, na classe de tratamento de erros; Simplificar o JSON devolvido pela API em casos de erro de validação do Bean Validation.

--- src/main/java/estudos/vollmed/infra/TratadorDeErros.java
+++ src/main/java/estudos/vollmed/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package estudos.vollmed.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,4 +15,15 @@ public class TratadorDeErros {
         return ResponseEntity.status(404).body("Recurso não encontrado 😐");
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.status(400).body(erros.stream().map(DadosErrosValidacao::new).toList());
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
 }
